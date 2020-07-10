3ab513b2c30679bc667ebb90b570e12b594ee6b1
Felipe Gonzalez <felipefagnilli@gmail.com>
2020-07-10 20:39:46 +0000

    function code

--- helpers.js
+++ helpers.js
@@ -1,12 +1,11 @@
 const findUser = (email, database) => {
   for (let user in database) {
     if (database[user].email === email) {
-      // console.log(user);
       return user;
-    } else {
-      return undefined;
     }
+    console.log(user);
   }
+  return undefined;
 };
 
 module.exports = { findUser };
